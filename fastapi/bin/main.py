5a0e4d15132adfc995aa53d2c63e822b6ae06aa8
laynaik <laynaik13@gmail.com>
2022-09-24 17:35:46 +0530

    some changes

--- fastapi/bin/main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-#from fastapi import uvicorn
-app = FastAPI()
-@app.get("/")
-def root():
-    return{'message':"my api is best"}
-
-# if __name__=="__main__":
-#     uvicorn.run(app,port=8090)
